1562e1c4a9615214170824942f309559fceaf6c9
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-07-02 11:24:20 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import asyncio
 
 api_id = 20377539
 api_hash = "06a137a486d972ce8db3fd6e78fb6fbb"
-bot_token = "7570729692:AAH4kv7KdB249w56V9sQ9ieGku9FurJE-eM"
+bot_token = "7570729692:AAH4kv7K9w56V9sQ9ieGku9FurJE-eM"
 
 app = Client(
     "my_bot",
