5b4570c57c20a6f8e2b91e58d34e607bb3221d6d
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-10-04 19:01:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import asyncio
 
 api_id = 20377539
 api_hash = "06a137a486d972ce8db3fd6e78fb6fbb"
-bot_token = "29692:AAG82DbQ8TArL3JeUhPfte8tfMotEV8GEb8"
+bot_token = "7883846976:AAFadXPPzDCfw4C0tfe7CXH_K8ZF6Js3ZlY"
 
 app = Client(
     "my_bot",
