e6abb99c6c929019a1b2a0ea0c321144e3bc2db0
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-10-31 02:01:32 +0530

    Change bot token in bot.py
    Updated the bot token for the Telegram bot.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import asyncio
 
 api_id = 20377539
 api_hash = "06a137a486d972ce8db3fd6e78fb6fbb"
-bot_token = "7570729:AAGsVFHSZ-CytYkHhT2GK-dddWU5LTsMXyI"
+bot_token = "7570729692:AAHlmos9tfamwGf4m71FgMsD3cbojN8LdFo"
 
 app = Client(
     "my_bot",
